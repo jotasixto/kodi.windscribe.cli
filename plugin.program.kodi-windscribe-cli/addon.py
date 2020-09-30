7b6e69e98a6578afcb900689528426a2f99c3928
Jota Sixto Anello <juan@sixtoanello.com>
2020-09-30 19:54:21 +0100

    Control location and conection

--- plugin.program.kodi-windscribe-cli/addon.py
+++ plugin.program.kodi-windscribe-cli/addon.py
@@ -11,12 +11,41 @@ enable		= addon.getSetting('enable')
 username	= addon.getSetting('username')
 password	= addon.getSetting('password')
 location	= addon.getSetting('location')
- 
- 
+
 def windscribe_login(param_username, param_password):
+    if username == "" :
+        return ""
+    
     p = Popen(['/usr/bin/windscribe', 'login'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
     inputlogin = username+"\n"+password+"\n"
     out, err = p.communicate(inputlogin.encode())
+    return_code = p.returncode
+    return out
+
+def windscribe_connect():
+    locations = ["best","GB","DE","NL","FR","US","CA"]
+    intlocation = 0
+    
+    try: 
+        intlocation = int(location)
+        
+        if (intlocation > len(locations)):
+            intlocation = 0
+    except ValueError:
+        pass
+    
+    p = Popen(['/usr/bin/windscribe', 'connect', locations[intlocation]], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    out, err = p.communicate()
+    return_code = p.returncode
+    return out
+    
+def windscribe_disconnect():
+    locations = ["best","GB","DE","NL","FR","US","CA"]
+    
+    intlocation = int(location)
+    
+    p = Popen(['/usr/bin/windscribe', 'disconnect'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    out, err = p.communicate()
     return_code = p.returncode
     return out
 
@@ -26,37 +55,26 @@ def windscribe_status():
     return_code = p.returncode
     return out
 
-
-if enable == "true":
-    with open("/tmp/pluginwscli.log", "a") as myfile:
-        myfile.write("plugin enable\n")
-        myfile.write(enable)
-        myfile.write(username)
-        myfile.write(password)
-        myfile.write(location)
-        myfile.write("======================**********====================\n")
-else:
-    with open("/tmp/pluginwscli.log", "a") as myfile:
-        myfile.write("plugin disable\n")
-        myfile.write(enable)
-        myfile.write(username)
-        myfile.write(password)
-        myfile.write(location)
-        myfile.write("======================**********====================\n")
-	
-line1 = "Windscribe Status:"
+pDialog = xbmcgui.DialogProgress()
+pDialog.create('Kodi', 'Windscribe...')
+pDialog.update(5, 'Windscribe updating...')
 
 status = windscribe_status()
 
 if enable == "true":
     if "DISCONNECTED" in status:
+        pDialog.update(35, 'Windscribe login...')
         windscribe_login(username, password)
-        
-        status = windscribe_status()
+
+    pDialog.update(55, 'Windscribe connecting...')
+    windscribe_connect()
 else:
     if "CONNECTED" in status:
-        status = windscribe_status()
+        pDialog.update(55, 'Windscribe desconnecting...')
+        windscribe_disconnect()
 
+
+pDialog.update(75, 'Windscribe status...')
 status = windscribe_status()
 
 xbmcgui.Dialog().ok(addonname, status)
